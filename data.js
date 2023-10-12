83597465feff47dc74df8019f85ce949db36233d
Sanju Kamat <SanjuKamat@gmail.com>
2023-10-12 12:54:57 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,125 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-way-to-beach-",
+      "name": "Way to beach ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.6328711288491604,
+        "pitch": 0.005474756499255662,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8838143768440894,
+          "pitch": 0.05377412080708588,
+          "rotation": 0,
+          "target": "1-way-to-beach"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-way-to-beach",
+      "name": "Way to beach",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.774721075114817,
+        "pitch": -0.03671310550186391,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1871912494173156,
+          "pitch": -0.07184319093911995,
+          "rotation": 0,
+          "target": "2-way-to-beach"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-way-to-beach",
+      "name": "Way to beach",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.6231769981019486,
+        "pitch": -0.054888014086884596,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8395149366048447,
+          "pitch": 0.046304680868775705,
+          "rotation": 0,
+          "target": "1-way-to-beach"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Way To Beach",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
